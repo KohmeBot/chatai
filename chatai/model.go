9acfd4f864590b0d7d054997adb1eca7d6ce9c27
xjh <threeax@foxmail.com>
2024-10-21 15:11:38 +0800

    fix: 第一次时插入数据

--- chatai/model.go
+++ chatai/model.go
@@ -1,6 +1,7 @@
 package chatai
 
 import (
+	"errors"
 	"gorm.io/gorm"
 	"time"
 )
@@ -15,13 +16,20 @@ type UsageRecord struct {
 
 // Allow 判断是否允许使用
 func (r *UsageRecord) Allow(db *gorm.DB, limitInput int64, limitOutput int64) (bool, error) {
-	err := db.First(&r).Error
-	if err != nil {
-		return false, err
-	}
-
 	now := time.Now()
 	todayMidnight := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location())
+	err := db.First(&r).Error
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			r.UseInputToken = 0
+			r.UseOutputToken = 0
+			r.LastUpdate = todayMidnight
+			if err := db.Save(&r).Error; err != nil {
+				return false, err
+			}
+		}
+	}
+
 	if todayMidnight.Sub(r.LastUpdate) >= 24*time.Hour {
 		// 如果 LastUpdate 过了24h，重置 token
 		r.UseInputToken = 0
