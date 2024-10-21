37de3cbb598cbff1785b71cb04ce51fbe63fdd73
xjh <threeax@foxmail.com>
2024-10-21 11:32:06 +0800

    feat: 完成基本功能

--- chatai/config.go
+++ chatai/config.go
@@ -11,8 +11,9 @@ type Config struct {
 	// 每个人每天的output token上限
 	InputToken int64 `mapstructure:"input_token"`
 	// 每人每天的output token上限
-	OutputToken int64  `mapstructure:"output_token"`
-	LimitTips   string `mapstructure:"limit_tips"`
+	OutputToken int64 `mapstructure:"output_token"`
+	// 达到上限后的提示词
+	LimitTips string `mapstructure:"limit_tips"`
 
 	// 控制模型是否联网，如果对应模型支持的话
 	Online bool `mapstructure:"online"`
