09dc33bfd63b6573f3c388339d9a2c6999224091
jhue <threeax@foxmail.com>
2026-08-06 18:47:02 +0800

    feat: just version

--- chatai/plugin.go
+++ chatai/plugin.go
@@ -142,4 +142,4 @@ func (c *ChatPlugin) OnInit(engine plugin.Engine, env plugin.Env) error {
 func (c *ChatPlugin) OnBoot()              {}
 func (c *ChatPlugin) OnHelp(ctx *zero.Ctx) {}
 func (c *ChatPlugin) Name() string         { return "chatai" }
-func (c *ChatPlugin) Version() string      { return "v1.0.4-beta" }
+func (c *ChatPlugin) Version() string      { return "v1.0.5" }
